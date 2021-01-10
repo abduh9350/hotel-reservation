abde6c515aa6819ba53a01d097b57a781bb94526
Abdu Jemal <abduh9350@gmail.com>
2021-01-09 23:54:04 -0500

    code improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,8 @@ import logo from '../images/logo.svg'
 import {FaAlignRight} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 function Navbar() {
-   let state={
-        isOpen: false
-    }
-   const handleToggle = () => {
-        this.setState({isOpen: !this.state.isOpen})
-    }
+  
+  
     return (
         <nav className='navbar'>
           <div className="nav-center">
